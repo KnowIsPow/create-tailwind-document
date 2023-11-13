14273dedd11306d5b52c636ca40bf46d8a47ca01
KnowIsPow <KnowIsPow@users.noreply.github.com>
2023-11-13 17:01:47 -0500

    Initial commit

---
+++ src/features/page.tsx
@@ -0,0 +1,7 @@
+export function Page({ children }) {
+  return (
+    <section className="bg-white border h-[66rem] w-[51rem] break-before-page mx-auto print:w-full print:h-screen print:border-none">
+      {children}
+    </section>
+  );
+}

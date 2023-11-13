f93b535f387f790894f7d340b45ea3a4a2bd29e8
KnowIsPow <KnowIsPow@users.noreply.github.com>
2023-11-13 17:22:37 -0500

    Remove large breakpoint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,15 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+    },
+  },
   plugins: [],
 };
 export default config;
